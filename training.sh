3117d44ef49de487b92e4acfdd40c91da6b02f40
zpyuan6 <zpyuan6@163.com>
2023-07-25 09:50:54 +0100

    add nema yaml

--- training.sh
+++ training.sh
@@ -8,4 +8,13 @@ python train.py --workers 0 --device 0 --batch-size 6 --data data\wheat_pest_new
 
 python train.py --workers 0 --device 0 --batch-size 6 --data data\our_dataset_all.yaml --img 640 640 --cfg cfg\training\yolov7_wheat_pest_all_classes.yaml --weights 'yolov7_training.pt' --name yolov7-our-dataset-all-classes
 
-python train.py --epochs 500 --workers 6 --device 0 --batch-size 6 --data data\disease_dataset.yaml --img-size 512 --cfg cfg\training\yolov7_disease_detection.yaml --weights 'runs\train\yolov7-disease-detection6\weights\best.pt' --name yolov7-disease-detection
+python train.py --epochs 500 --workers 6 --device 0 --batch-size 6 --data data\disease_dataset.yaml --img-size 512 --cfg cfg\training\yolov7_disease_detection.yaml --weights 'yolov7.pt' --name yolov7-disease-detection
+
+
+python train.py --epochs 400 --workers 6 --device 0 --batch-size 6 --data data\AgriNema.yaml --img-size 512 --cfg cfg\training\yolov7_AgriNema.yaml --weights 'yolov7.pt' --name agrinema
+
+python train.py --epochs 400 --workers 6 --device 0 --batch-size 6 --data data\BBBC010.yaml --img-size 512 --cfg cfg\training\yolov7_BBBC010.yaml --weights 'yolov7.pt' --name bbbc010
+
+python train.py --epochs 400 --workers 6 --device 0 --batch-size 6 --data data\Celegans.yaml --img-size 512 --cfg cfg\training\yolov7_Celegans.yaml --weights 'yolov7.pt' --name celegans
+
+python train.py --epochs 400 --workers 6 --device 0 --batch-size 6 --data data\microorganism.yaml --img-size 512 --cfg cfg\training\yolov7_Microorganism.yaml --weights 'yolov7.pt' --name microorganism
